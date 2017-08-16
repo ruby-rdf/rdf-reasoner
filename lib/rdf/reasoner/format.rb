66f96016d862c605ed4098207a45d9244ea493ff
Gregg Kellogg <gregg@greggkellogg.net>
2017-08-16 15:11:11 -0700

    Update CLI.

--- lib/rdf/reasoner/format.rb
+++ lib/rdf/reasoner/format.rb
@@ -14,33 +14,31 @@ module RDF::Reasoner
     def self.cli_commands
       {
         entail: {
-          description: "Perform RDFS entailment to expand the repository based on referenced built-in vocabuaries",
-          help: "entail",
+          description: "Add entailed triples to repository",
+          help: "entail\nPerform RDFS, OWL and schema.org entailment to expand the repository based on referenced built-in vocabuaries",
+          control: :button, # Treats this like a separate control in the HTML UI
           parse: true,
           lambda: ->(argv, opts) do
             RDF::Reasoner.apply(:rdfs, :owl, :schema)
             start, stmt_cnt = Time.now, RDF::CLI.repository.count
             RDF::CLI.repository.entail!
             secs, new_cnt = (Time.new - start), (RDF::CLI.repository.count - stmt_cnt)
-            $stdout.puts "\nEntailed #{new_cnt} new statements in #{secs} seconds."
+            opts[:logger].info "\nEntailed #{new_cnt} new statements in #{secs} seconds."
           end
         },
         lint: {
-          description: "Lint the repository using built-in vocabularies",
-          help: "lint",
+          description: "Lint the repository",
+          help: "lint\nLint the repository using built-in vocabularies",
           parse: true,
+          option_use: {output_format: :disabled},
           lambda: ->(argv, opts) do
             RDF::Reasoner.apply(:rdfs, :owl, :schema)
             start = Time.now
-            messages = RDF::CLI.repository.lint
+            # Messages added to options for appropriate display
+            options[:messages].merge!(RDF::CLI.repository.lint)
+            $opts[:output].puts "Linter responded with #{options[:messages].empty? ? 'no' : ''} messages."
             secs = Time.new - start
-            messages.each do |kind, term_messages|
-              term_messages.each do |term, messages|
-                $stdout.puts "#{kind}  #{term}"
-                messages.each {|m| $stdout.puts "  #{m}"}
-              end
-            end
-            $stdout.puts "\nLinted in #{secs} seconds."
+            opts[:logger].info "\nLinted in #{secs} seconds."
           end
         }
       }
