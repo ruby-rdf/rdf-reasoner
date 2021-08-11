43f07777ae5f423f3fdea7051bda4d979b1383ca
Gregg Kellogg <gregg@greggkellogg.net>
2021-08-11 11:38:23 -0700

    CI update for coveralls.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,15 +15,21 @@ require 'rdf/xsd'
 
 begin
   require 'simplecov'
-  require 'coveralls'
+  require 'simplecov-lcov'
+
+  SimpleCov::Formatter::LcovFormatter.config do |config|
+    #Coveralls is coverage by default/lcov. Send info results
+    config.report_with_single_file = true
+    config.single_report_path = 'coverage/lcov.info'
+  end
+
   SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
     SimpleCov::Formatter::HTMLFormatter,
-    Coveralls::SimpleCov::Formatter
+    SimpleCov::Formatter::LcovFormatter
   ])
   SimpleCov.start do
     add_filter "/spec/"
   end
-  Coveralls.wear!
 rescue LoadError
 end
 
